1eaa0da0e7d089a6950c3be9729b1e03126a27bf
Andy Kaminski <andy.kaminski42@gmail.com>
2022-10-27 18:59:44 -0400

    Added /profile route to homeroutes. Can't test if this works yet, or if its right, unfortunately

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -33,5 +33,25 @@ router.get('/users/:id', authUser, async (req, res) => {
   }
 })
 
+// Hopefully this works
+router.get('/profile', authUser, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.userId, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Item }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 module.exports = router;
