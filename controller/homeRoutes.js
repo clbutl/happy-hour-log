44c174476a440a2e8dd7eccb7d164bf8cc187d6e
Edna Omadjambe <ednaomadjambe@Ednas-MacBook-Air.local>
2022-10-27 20:40:11 -0400

    switched model from user to item

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -19,20 +19,17 @@ const upload = multer({ storage: storage })
 
 router.get('/', async (req, res) => {
   try {
-    // Get all users, sorted by name
-    const userData = await User.findAll({
-      include: [{
-        model: Item, Location, 
-        attributes: ['name', 'price', 'originalPrice', 'dealDay', 'dealType', 'restaurantName' ]
-      }], 
+   
+    const itemData = await Item.findAll({
+      include: [{model: User, Location}], 
       // order: [['name', 'DSC']],
     });
 
     // Serialize user data so templates can read it
-  const users = userData.map((user) => user.get({ plain: true }));
+  const items = itemData.map((item) => item.get({ plain: true }));
 
-    // Pass serialized data into Handlebars.js template
-    res.render('homepage', { users });
+  // Pass serialized data into Handlebars.js template
+    res.render('homepage', { items });
   } catch (err) {
     res.status(500).json(err);
   }
