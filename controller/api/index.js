30fc84c85d532041fa866c470fd0d83a96333c32
clbutl <cannonlbutler@gmail.com>
2022-11-01 13:24:20 -0400

    purged upload routes and handlebars, added user seeds that work

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const uploadRoutes = require('./uploadRoutes');
 const foodRoutes = require('./foodRoutes');
 const drinkRoutes = require('./drinkRoutes');
 const itemRoutes = require('./itemRoutes');
@@ -9,7 +8,6 @@ const locationRoutes = require('./locationRoutes')
 
 
 router.use('/users', userRoutes);
-router.use('/upload', uploadRoutes);
 router.use('/food', foodRoutes);
 router.use('/drink', drinkRoutes);
 router.use('/item', itemRoutes);
