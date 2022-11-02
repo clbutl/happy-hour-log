da3554089b03d4eac2b3f8e538a54dab5a02ccd6
clbutl <cannonlbutler@gmail.com>
2022-11-02 09:36:50 -0400

    added dropdown

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,55 @@
-
-
-// console.log('hello')
-
-// document
-//   .querySelector('#profile-pic')
-//   .addEventListener('click', function(event) {
-//     console.log(req.session.user_username)
-//     event.preventDefault()
-//     document.location.replace(`/profile/${req.session.user_username}`)
-//   });
-
-if (document.querySelector('.login-btn')) {
-  document
-  .querySelector('.login-btn')
-  .addEventListener('click', function(event) {
-  event.preventDefault()
-  document.location.replace('/login')
-})
-} else {
-  document
-  .querySelector('.profile-btn')
-  .addEventListener('click', function(event) {
-  event.preventDefault()
-  document.location.replace('/profile')
-})
-}
-
-const goHome = () => {
+const homeBtn = () => {
   document.location.replace('/')
 }
 
 document.querySelector('#home-btn')
-.addEventListener('click', goHome)
+.addEventListener('click', homeBtn)
 
 
+const dropdownList = document.querySelector('.dropdown-content-main')
+const showDropdown = () => {
+  if (dropdownList.classList.contains('hidden')) {
+    dropdownList.classList.remove('hidden')
+  } else {
+    dropdownList.classList.add('hidden')
+  }
+}
 
+document.querySelector('#profile-pic').addEventListener('click', showDropdown)
 
+const goProfile = () => {
+  dropdownList.classList.add('hidden')
+  document.location.replace('/profile')
+}
+const goPost = () => {
+  dropdownList.classList.add('hidden')
+  document.location.replace('/post')
+}
+const logOut = async (event) => {
+  event.preventDefault();
+
+  if (document.location.href === 'http://localhost:3001/profile' || document.location.href === 'http://localhost:3001/post') {
+    await fetch('/api/users/logout', {
+      method: 'POST',
+    });
+  } else {
+    document.location.replace('/profile')
+    await fetch('/api/users/logout', {
+      method: 'POST',
+    });
+  }
+  dropdownList.classList.add('hidden')
+  document.location.replace(`/login`);
+}
+const login = () => {
+  dropdownList.classList.add('hidden')
+  document.location.replace(`/login`);
+}
+
+if (document.querySelector('#main-login-btn')) {
+  document.querySelector('#main-login-btn').addEventListener('click', login)
+} else {
+  document.querySelector('#main-profile-btn').addEventListener('click', goProfile)
+  document.querySelector('#main-create-btn').addEventListener('click', goPost)
+  document.querySelector('#main-logout-btn').addEventListener('click', logOut)
+}
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,23 +1,21 @@
-const logOut = async (event) => {
-  event.preventDefault();
-  console.log(document.location.href)
+// const logOut = async (event) => {
+//   event.preventDefault();
+//   console.log(document.location.href)
 
-  if (document.location.href === 'http://localhost:3001/profile') {
-    await fetch('/api/users/logout', {
-      method: 'POST',
-    });
-  } else {
-    document.location.replace('/profile')
-    await fetch('/api/users/logout', {
-      method: 'POST',
-    });
-  }
-  document.location.replace(`/login`);
+//   if (document.location.href === 'http://localhost:3001/profile') {
+//     await fetch('/api/users/logout', {
+//       method: 'POST',
+//     });
+//   } else {
+//     document.location.replace('/profile')
+//     await fetch('/api/users/logout', {
+//       method: 'POST',
+//     });
+//   }
+//   document.location.replace(`/login`);
+// }
 
-  
-}
-
-document
-  .querySelector('#logout-btn')
-  .addEventListener('click', logOut);
+// document
+//   .querySelector('#logout-btn')
+//   .addEventListener('click', logOut);
 
